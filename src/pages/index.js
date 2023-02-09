61d3ec19c57c49127fcd730a64a263cd84753ff7
Pedro Monteiro <pmapm@ua.pt>
2023-02-09 15:48:06 +0000

    change button text on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Plataforma Tutorial - 5min ⏱️
+            Plataforma
           </Link>
         </div>
       </div>
